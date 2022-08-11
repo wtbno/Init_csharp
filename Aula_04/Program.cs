52bd70b8cd3acc2ee9ead51145976225c3dfa898
Bruno Alves <bruno_links@hotmail.com>
2022-08-11 20:14:32 -0300

    Feat.: Aula break e continue

---
+++ Aula_04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Boolean condicao = true;
+            int valor;
+            do
+            {
+                System.Console.WriteLine("Digite um valor ou insira 0 para sair");
+                valor = int.Parse(Console.ReadLine());
+
+                if (valor == 0)
+                {
+                    System.Console.WriteLine("Você saiu da aplicação");
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine("O valor inserido foi: " + valor);
+                    continue;
+                }
+            } while (condicao == true);
+        }
+    }
+}
