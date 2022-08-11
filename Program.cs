1cb1985b457f71777bbd34180d09dd3aa480690e
Bruno Alves <bruno_links@hotmail.com>
2022-08-11 19:05:27 -0300

    aula 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Olá, mundo!!");
+﻿using System;
+
+class program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int valor1;
+        int valor2;
+        System.Console.WriteLine("Digite o primeiro valor: ");
+        //Conversão de string para int
+        valor1 = int.Parse(Console.ReadLine());
+        System.Console.WriteLine("Digite o segundo valor: ");
+        valor2 = int.Parse(Console.ReadLine());
+
+        int soma = valor1 + valor2;
+        System.Console.WriteLine("A soma dos valores foram: " + soma);
+    }
+
+}
